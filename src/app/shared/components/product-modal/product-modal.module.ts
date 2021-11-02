88d15f52a405c393293966a4491c79a064e4abd3
Anita Rosikhina <anita2.sergeevna@gmail.com>
2021-11-02 16:40:16 +0200

    - updated product-modal design - added responsive product-modal design

--- src/app/shared/components/product-modal/product-modal.module.ts
+++ src/app/shared/components/product-modal/product-modal.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import {ProductModalComponent} from "./product-modal.component";
 import {CommonButtonModule} from "../common-button/common-button.module";
 import {MatDialogModule} from "@angular/material/dialog";
+import {ProductsDetailsRoutingModule} from "../../../pages/products-details/products-details-routing.module";
 
 @NgModule({
   declarations: [ProductModalComponent],
   imports: [
     CommonModule,
     CommonButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    ProductsDetailsRoutingModule
   ]
 })
 export class ProductModalModule { }
