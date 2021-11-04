a0403ec1d35efc4cf6823a510423e57688dbf710
Anita Rosikhina <anita2.sergeevna@gmail.com>
2021-11-04 08:47:41 +0200

    - added routing admin-layout

--- src/app/layouts/admin/admin-layout-routing.module.ts
+++ src/app/layouts/admin/admin-layout-routing.module.ts
@@ -3,7 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import {AdminLayoutComponent} from "./admin-layout.component";
 
 const routes: Routes = [
-  {path: '', component: AdminLayoutComponent}
+  {path: '', component: AdminLayoutComponent, children: [
+      {
+        path: 'subcategories-addition',
+        loadChildren: () => import('./pages/subcategories-addition-page/subcategories-addition-page.module').then(m => m.SubcategoriesAdditionPageModule)
+      },
+      {
+        path: 'product-addition',
+        loadChildren: () => import('./pages/product-addition-page/product-addition-page.module').then(m => m.ProductAdditionPageModule)
+      },
+      {path: '', redirectTo: 'product-addition'}
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/layouts/admin/admin-layout.module.ts
+++ src/app/layouts/admin/admin-layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { AdminLayoutRoutingModule } from './admin-layout-routing.module';
 import {AdminLayoutComponent} from "./admin-layout.component";
+import {AdminHeaderComponent} from "./components/admin-header/admin-header.component";
+import {CommonButtonModule} from "../../shared/components/common-button/common-button.module";
 
 
 @NgModule({
-  declarations: [AdminLayoutComponent],
-  imports: [AdminLayoutRoutingModule]
+  declarations: [AdminLayoutComponent, AdminHeaderComponent],
+  imports: [AdminLayoutRoutingModule, CommonButtonModule]
 })
 export class AdminLayoutModule { }
