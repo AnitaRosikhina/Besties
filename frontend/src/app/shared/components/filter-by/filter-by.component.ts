9c6e4e70550c9c024d03185a3721cad04d743619
Anita Rosikhina <anita2.sergeevna@gmail.com>
2021-11-08 18:02:13 +0200

    - created filter-by component - added styles

--- frontend/src/app/shared/components/filter-by/filter-by.component.ts
+++ frontend/src/app/shared/components/filter-by/filter-by.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {Options} from "@angular-slider/ngx-slider";
 
 @Component({
   selector: 'app-filter-by',
@@ -7,7 +8,13 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FilterByComponent implements OnInit {
-
+  @Input() subcategories: string[]
+  value: number = 40;
+  highValue: number = 60;
+  options: Options = {
+    floor: 0,
+    ceil: 100
+  };
   constructor() { }
 
   ngOnInit(): void {
--- frontend/src/app/shared/components/filter-by/filter-by.module.ts
+++ frontend/src/app/shared/components/filter-by/filter-by.module.ts
@@ -1,8 +1,21 @@
 import {NgModule} from '@angular/core';
 import {FilterByComponent} from "./filter-by.component";
-
+import {MatExpansionModule} from "@angular/material/expansion";
+import {NgxSliderModule} from "@angular-slider/ngx-slider";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [FilterByComponent],
+  exports: [
+    FilterByComponent
+  ],
+  imports: [
+    MatExpansionModule,
+    NgxSliderModule,
+    FormsModule,
+    CommonModule,
+  ]
 })
-export class FilterByModule { }
+export class FilterByModule {
+}
