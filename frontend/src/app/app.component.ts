03b4658c95988c7df739e8048fe281619e5ff2a2
Anita Rosikhina <anita2.sergeevna@gmail.com>
2021-11-12 22:56:43 +0200

    - minor fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,7 +3,6 @@ import {JwtHelperService} from "@auth0/angular-jwt";
 import {TOKEN_NAME} from "./shared/constants/tokens-name";
 import {timer} from "rxjs";
 import {LoginService} from "./shared/services/login.service";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -13,14 +12,12 @@ import {Router} from "@angular/router";
 })
 export class AppComponent implements OnInit {
   constructor(private jwtHelperService: JwtHelperService,
-              private loginService: LoginService,
-              private router: Router) {}
+              private loginService: LoginService) {}
 
   ngOnInit(): void {
     timer(0, 60_000).subscribe(() => {
       if (this.jwtHelperService.isTokenExpired()) {
         this.loginService.logout()
-        this.router.navigate([''])
       } else {
         localStorage.setItem(TOKEN_NAME, this.jwtHelperService.tokenGetter())
       }
